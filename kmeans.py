5fcf0624abe8def9a0d1fdde9f154a874af787e4
HcwXd <e04dl47878@gmail.com>
2018-06-29 16:43:28 +0800

    Update data generate to fit clusters

--- kmeans.py
+++ kmeans.py
@@ -10,30 +10,64 @@ Step5. Repeat Step3 and Step4 til the world end
 """
 
 
-number_of_data = 100
-number_of_ans = 3
-answer = [[3000, 2000], [8000, 5000], [4000, 7000]]
-ans_x = []
-ans_y = []
+def data_generation():
+    number_of_data = 1000
+    number_of_ans = 3
+    answer = [[30000, 20000], [80000, 50000], [40000, 70000]]
+    ans_x = []
+    ans_y = []
 
-data_x = []
-data_y = []
+    data_x = []
+    data_y = []
 
-for i in answer:
-    ans_x.append(i[0])
-    ans_y.append(i[1])
+    for i in answer:
+        ans_x.append(i[0])
+        ans_y.append(i[1])
 
-for i in range(number_of_data):
-    for j in range(number_of_ans):
-        error_nx = random.randint(-2000, 2000)
-        error_ny = random.randint(-2000, 2000)
-        error_px = random.randint(-2000, 2000)
-        error_py = random.randint(-2000, 2000)
-        data_x.append(ans_x[j] + random.randint(-2000 +
-                                                error_nx, 2000 + error_ny))
-        data_y.append(ans_y[j] + random.randint(-2000 +
-                                                error_px, 2000 + error_py))
+    for i in range(int(number_of_data/2)):
+        for j in range(number_of_ans):
+            error_nx = random.randint(-10000, 10000)
+            error_ny = random.randint(-10000, 10000)
+            error_px = random.randint(-10000, 10000)
+            error_py = random.randint(-10000, 10000)
+            data_x.append(ans_x[j] + random.randint(-20000 +
+                                                    error_nx, 20000 + error_ny))
+            data_y.append(ans_y[j] + random.randint(-20000 +
+                                                    error_px, 20000 + error_py))
+    for i in range(int(number_of_data/(5/2))):
+        for j in range(number_of_ans):
+            error_nx = random.randint(-10000, 10000)
+            error_ny = random.randint(-10000, 10000)
+            error_px = random.randint(-10000, 10000)
+            error_py = random.randint(-10000, 10000)
+            data_x.append(ans_x[j] + random.randint(-10000 +
+                                                    error_nx, 10000 + error_ny))
+            data_y.append(ans_y[j] + random.randint(-10000 +
+                                                    error_px, 10000 + error_py))
+    for i in range(int(number_of_data/(5/2))):
+        for j in range(number_of_ans):
+            error_nx = random.randint(-10000, 10000)
+            error_ny = random.randint(-10000, 10000)
+            error_px = random.randint(-10000, 10000)
+            error_py = random.randint(-10000, 10000)
+            data_x.append(ans_x[j] + random.randint(-20000 +
+                                                    error_nx, 20000 + error_ny))
+            data_y.append(ans_y[j] + random.randint(-20000 +
+                                                    error_px, 20000 + error_py))
+    for i in range(int(number_of_data/(5/1))):
+        for j in range(number_of_ans):
+            error_nx = random.randint(-20000, 20000)
+            error_ny = random.randint(-20000, 20000)
+            error_px = random.randint(-20000, 20000)
+            error_py = random.randint(-20000, 20000)
+            data_x.append(ans_x[j] + random.randint(-20000 +
+                                                    error_nx, 20000 + error_ny))
+            data_y.append(ans_y[j] + random.randint(-20000 +
+                                                    error_px, 20000 + error_py))
+
+    plt.scatter(data_x, data_y, color="blue", s=3)
+    plt.scatter(ans_x, ans_y, color="red")
+    return data_x, data_y
 
 
-plt.scatter(data_x, data_y, color="blue")
-plt.scatter(ans_x, ans_y, color="red")
+data_x, data_y = data_generation()
