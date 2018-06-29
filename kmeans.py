ab213a6f9074f94c87de3f60b6d984bc60750469
HcwXd <e04dl47878@gmail.com>
2018-06-29 15:25:47 +0800

    Upload the procedure

---
+++ kmeans.py
@@ -0,0 +1,9 @@
+
+"""
+Step0. Read data(X1, X2, X3, Xi)
+Step1. Randomly pick K random points as cluster centers called centroids.
+Step3. Assign each data to the clusters by its distance
+Step4. Pick new centroids by the clusters
+Step5. Repeat Step3 and Step4 til the world end
+"""
+
